9ca885706f26810122f3f1bd017d46fcb436554c
Luis Nuñez <luisalbertonu@gmail.com>
2019-09-25 20:07:41 -0300

    User Class

--- MyLeasing.Web/Data/DataContext.cs
+++ MyLeasing.Web/Data/DataContext.cs
@@ -1,12 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using MyLeasing.Web.Data.Entities;
 namespace MyLeasing.Web.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<User>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
+        public DbSet<Manager> Managers { get; set; }
         public DbSet<Owner> Owners { get; set; }
         public DbSet<Contract> Contracts { get; set; }
         public DbSet<Lessee> Lessees { get; set; }
