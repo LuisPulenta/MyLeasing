f80e7638215919d89bcac4e81cec2de86745814f
Luis Nuñez <luisalbertonu@gmail.com>
2019-11-05 19:12:42 -0300

    Adicionando Estilos

--- MyLeasing.Common/Helpers/Settings.cs
+++ MyLeasing.Common/Helpers/Settings.cs
@@ -5,6 +5,9 @@ namespace MyLeasing.Common.Helpers
 {
     public static class Settings
     {
+        private const string _token = "token";
+        private const string _owner = "owner";
+
         private const string _propertyImages = "PropertyImages";
         private const string _property = "property";
         private static readonly string _settingsDefault = string.Empty;
@@ -21,5 +24,18 @@ namespace MyLeasing.Common.Helpers
             get => AppSettings.GetValueOrDefault(_property, _settingsDefault);
             set => AppSettings.AddOrUpdateValue(_property, value);
         }
+
+        public static string Token
+        {
+            get => AppSettings.GetValueOrDefault(_token, _settingsDefault);
+            set => AppSettings.AddOrUpdateValue(_token, value);
+        }
+
+        public static string Owner
+        {
+            get => AppSettings.GetValueOrDefault(_owner, _settingsDefault);
+            set => AppSettings.AddOrUpdateValue(_owner, value);
+        }
+
     }
 }
